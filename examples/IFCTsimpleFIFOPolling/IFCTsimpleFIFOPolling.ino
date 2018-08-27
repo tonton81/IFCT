bd85c5ad1abf93a0d5998902822c12c5444d370a
Antonio Brewer <yodabox64@hotmail.com>
2018-08-27 14:37:14 -0400

    Add files via upload

---
+++ examples/IFCTsimpleFIFOPolling/IFCTsimpleFIFOPolling.ino
@@ -0,0 +1,25 @@
+#include <IFCT.h>
+
+void setup() {
+  pinMode(2, OUTPUT); // for the transceiver enable pin
+  Can0.setBaudRate(1000000);
+  Can0.enableFIFO();
+}
+
+void loop() {
+  CAN_message_t msg;
+  if ( Can0.read(msg) ) canSniff(msg);
+}
+
+void canSniff(const CAN_message_t &msg) {
+  Serial.print("MB "); Serial.print(msg.mb);
+  Serial.print("  LEN: "); Serial.print(msg.len);
+  Serial.print(" EXT: "); Serial.print(msg.flags.extended);
+  Serial.print(" REMOTE: "); Serial.print(msg.rtr);
+  Serial.print(" TS: "); Serial.print(msg.timestamp);
+  Serial.print(" ID: "); Serial.print(msg.id, HEX);
+  Serial.print(" Buffer: ");
+  for ( uint8_t i = 0; i < msg.len; i++ ) {
+    Serial.print(msg.buf[i], HEX); Serial.print(" ");
+  } Serial.println();
+}
